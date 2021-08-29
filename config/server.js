1c7a129b7bca8f5b26ccfe3f25de105f54e9339b
pardeeep <pardeep.iwebcode@gmail.com>
2021-08-29 22:30:25 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bef2ea159059cd4035b1851a9dc9f754'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74555921-9ab7-4bdb-8d9e-f8991a783d84'
+};
